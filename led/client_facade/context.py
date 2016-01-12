3a8e837c517ac8efe2e51a9e586e3c0b5eead0b8
kees <cez.bloch@gmail.com>
2016-01-12 18:23:18 +0100

    Adding logger initialization from logging.json config file with multiple handlers writing to files

--- led/client_facade/context.py
+++ led/client_facade/context.py
@@ -1,6 +1,7 @@
 from connection_provider import ConnectionProvider
 from display import Display
 from imaging.effect_provider import EffectProvider
+from logger_provider import LoggerProvider
 from settings_provider import SettingsProvider
 from settings_defines import *
 
@@ -9,10 +10,11 @@ class ApplicationContext(object):
     instance = None
 
     def __init__(self):
-        self._connection_provider = ConnectionProvider()
-        self._display = Display()
-        self._effect_provider = EffectProvider()
-        self._settings_provider = SettingsProvider()
+        self.__logger_provider = LoggerProvider()
+        self.__connection_provider = ConnectionProvider()
+        self.__display = Display()
+        self.__effect_provider = EffectProvider()
+        self.__settings_provider = SettingsProvider()
 
     @staticmethod
     def get_instance():
@@ -20,31 +22,36 @@ class ApplicationContext(object):
             ApplicationContext.instance = ApplicationContext()
         return ApplicationContext.instance
 
+    def get_logger_provider(self):
+        return self.__logger_provider
+
     def get_connection_provider(self):
-        return self._connection_provider
+        return self.__connection_provider
 
     def get_display(self):
-        return self._display
+        return self.__display
 
     def get_effect_provider(self):
-        return self._effect_provider
+        return self.__effect_provider
 
     def get_settings_provider(self):
-        return self._settings_provider
+        return self.__settings_provider
 
     def startup(self):
-        config = self._settings_provider.get_config()
+        self.__logger_provider.setup_logging()
+
+        config = self.__settings_provider.get_config()
         reconnect_on_startup = config.getboolean(CONNECTION, RECONNECT_ON_STARTUP)
         if reconnect_on_startup:
             ip_address = config.get('connection', 'ip_address')
             port = config.getint('connection', 'port_number')
-            self._connection_provider.connect(ip_address, port)
+            self.__connection_provider.connect(ip_address, port)
             self._set_display_size()
 
     def _set_display_size(self):
-        config = self._settings_provider.get_config()
-        if self._connection_provider.is_connected():
+        config = self.__settings_provider.get_config()
+        if self.__connection_provider.is_connected():
             width = config.getint('panel', 'width')
             height = config.getint('panel', 'height')
-            self._display.set_size((width, height))
-            self._connection_provider.get_client().send_set_size(self._display.get_size())
+            self.__display.set_size((width, height))
+            self.__connection_provider.get_client().send_set_size(self.__display.get_size())
---
+++ led/client_facade/logger_provider.py
@@ -0,0 +1,17 @@
+import os
+import json
+import logging.config
+
+
+class LoggerProvider(object):
+    def setup_logging(self, default_path='logging.json', default_level=logging.INFO, env_key='LOG_CFG'):
+        path = default_path
+        value = os.getenv(env_key, None)
+        if value:
+            path = value
+        if os.path.exists(path):
+            with open(path, 'rt') as f:
+                config = json.load(f)
+            logging.config.dictConfig(config)
+        else:
+            logging.basicConfig(level=default_level)
