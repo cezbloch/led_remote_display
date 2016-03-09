6db1d135c9f96f14e681ef8ea7d51d71724ef4fb
kees <cez.bloch@gmail.com>
2016-03-09 23:13:50 +0100

    Make real server use reactor and twisted

--- led/apps/led_server.py
+++ led/apps/led_server.py
@@ -2,16 +2,18 @@ import sys
 sys.path.append('/home/pi/keesware/led')
 
 from device.device import Ws281xDevice
-from networking.factories import ServerFactory
+from networking.twisted_server import LedServer
 from device.strip_factory import StripeFactory
 
 
 def start_real_led_server():
     led_device = Ws281xDevice(StripeFactory(), (30, 10))
-    server = ServerFactory.create_server(led_device, address="192.168.1.4", port=6666)
-    raw_input("Press Enter to continue...")
+    server = LedServer(led_device)
+    server.start(6666)
     led_device.clear()
-    ServerFactory.clean_up_server(server)
+
+    from twisted.internet import reactor
+    reactor.run()
 
 if __name__ == '__main__':
     start_real_led_server()
